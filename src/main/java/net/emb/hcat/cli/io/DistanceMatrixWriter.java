ff6038c6b1cb78e1fdcbc29bf0cfc77a12b1b057
OT Piccolo <OT_Piccolo@web.de>
2021-04-11 16:03:02 +0200

    Can now save DistanceMatrix

---
+++ src/main/java/net/emb/hcat/cli/io/DistanceMatrixWriter.java
@@ -0,0 +1,114 @@
+package net.emb.hcat.cli.io;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Map;
+
+import net.emb.hcat.cli.haplotype.DistanceMatrix;
+import net.emb.hcat.cli.haplotype.Haplotype;
+
+/**
+ * Writes out the distance matrix in text form.
+ *
+ * @author OT Piccolo
+ */
+public class DistanceMatrixWriter {
+
+	private static final String HEADER = "HT-ID";
+
+	// Indents the given Appendable by the given amount of spaces.
+	private static void indent(final int times, final Appendable appendable) throws IOException {
+		for (int i = 0; i < times; i++) {
+			appendable.append(' ');
+		}
+	}
+
+	private final BufferedWriter writer;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param writer
+	 *            The writer data should be written to.
+	 */
+	public DistanceMatrixWriter(final Writer writer) {
+		if (writer == null) {
+			throw new IllegalArgumentException("Writer can't be null.");
+		}
+		this.writer = writer instanceof BufferedWriter ? (BufferedWriter) writer : new BufferedWriter(writer, 1024);
+	}
+
+	/**
+	 * Writes the distance matrix.
+	 *
+	 * @param matrix
+	 *            The distance matrix to write.
+	 * @throws IOException
+	 *             An I/O exception.
+	 */
+	public void write(final DistanceMatrix matrix) throws IOException {
+		final Map<Haplotype, Map<Haplotype, Integer>> matrixMap = matrix.getMatrix();
+
+		// Compute maximum length of names, so indentation can happen.
+		int maxLength = HEADER.length();
+		for (final Haplotype haplotype : matrixMap.keySet()) {
+			final String name = haplotype.getName();
+			maxLength = Math.max(maxLength, name != null ? name.length() : 4);
+		}
+
+		// Writing header line.
+		writer.append(HEADER);
+		indent(maxLength - HEADER.length(), writer);
+		for (final Haplotype haplotype : matrixMap.keySet()) {
+			writer.append('\t');
+			final String name = haplotype.getName();
+			writer.append(name);
+
+			final int length = name != null ? name.length() : 4;
+			if (length < 4) {
+				indent(4 - length, writer);
+			}
+		}
+		writer.newLine();
+		writer.flush();
+
+		// Writing data.
+		for (final Haplotype haplotype : matrixMap.keySet()) {
+			final String name = haplotype.getName();
+			writer.append(name);
+			final int length = name != null ? name.length() : 4;
+			indent(maxLength - length, writer);
+			final Map<Haplotype, Integer> distanceMap = matrixMap.get(haplotype);
+			for (final Haplotype otherHaplotype : matrixMap.keySet()) {
+				writer.append('\t');
+				final String otherName = otherHaplotype.getName();
+				final int otherLength = Math.max(4, otherName != null ? otherName.length() : 4);
+				if (haplotype == otherHaplotype) {
+					writer.append('-');
+					indent(otherLength - 1, writer);
+				} else {
+					final Integer distance = distanceMap.get(otherHaplotype);
+					final String distanceString = distance.toString();
+					writer.append(distanceString);
+					indent(otherLength - distanceString.length(), writer);
+				}
+			}
+			writer.newLine();
+			writer.flush();
+		}
+	}
+
+	/**
+	 * Convenience method to close the underlying writer.
+	 *
+	 * @see Writer#close()
+	 */
+	public void close() {
+		try {
+			writer.close();
+		} catch (final IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/test/java/ut/net/emb/hcat/cli/io/DistanceMatrixWriterTest.java
@@ -0,0 +1,83 @@
+package ut.net.emb.hcat.cli.io;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import net.emb.hcat.cli.haplotype.DistanceMatrix;
+import net.emb.hcat.cli.haplotype.Haplotype;
+import net.emb.hcat.cli.io.DistanceMatrixWriter;
+import net.emb.hcat.cli.sequence.Sequence;
+
+@SuppressWarnings("javadoc")
+public class DistanceMatrixWriterTest {
+
+	private static final Sequence MASTER_SEQUENCE = new Sequence("ABCDE", "Master");
+	private static final Sequence MID_DIFF_SEQUENCE = new Sequence("ABBDE", "Mid");
+	private static final Sequence FRONT_BACK_DIFF_SEQUENCE = new Sequence("BBCDD", "FrontAndBack");
+
+	@Test
+	public void testWriter() throws Exception {
+		final List<Haplotype> haplotypes = Haplotype.wrap(Arrays.asList(MASTER_SEQUENCE, MID_DIFF_SEQUENCE, FRONT_BACK_DIFF_SEQUENCE));
+		final DistanceMatrix matrix = new DistanceMatrix(haplotypes);
+
+		final ByteArrayOutputStream boas = new ByteArrayOutputStream(100);
+		final DistanceMatrixWriter writer = new DistanceMatrixWriter(new OutputStreamWriter(boas));
+		writer.write(matrix);
+
+		final StringBuilder builder = new StringBuilder(200);
+		builder.append("HT-ID\tHap01\tHap02\tHap03").append(System.lineSeparator());
+		builder.append("Hap01\t-    \t1    \t2    ").append(System.lineSeparator());
+		builder.append("Hap02\t1    \t-    \t3    ").append(System.lineSeparator());
+		builder.append("Hap03\t2    \t3    \t-    ").append(System.lineSeparator());
+
+		Assert.assertEquals(builder.toString(), boas.toString());
+	}
+
+	@Test
+	public void testWriterBigDiff() throws Exception {
+		final Haplotype hap1 = new Haplotype("Alphabetic");
+		hap1.add(new Sequence("ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+		final Haplotype hap2 = new Haplotype("BackwardAlphabetic");
+		hap2.add(new Sequence("ZYXWVUTSRQPONMLKJIHGFEDCBA"));
+		final DistanceMatrix matrix = new DistanceMatrix(Arrays.asList(hap1, hap2));
+
+		final ByteArrayOutputStream boas = new ByteArrayOutputStream(200);
+		final DistanceMatrixWriter writer = new DistanceMatrixWriter(new OutputStreamWriter(boas));
+		writer.write(matrix);
+
+		final StringBuilder builder = new StringBuilder(200);
+		builder.append("HT-ID             \tAlphabetic\tBackwardAlphabetic").append(System.lineSeparator());
+		builder.append("Alphabetic        \t-         \t26                ").append(System.lineSeparator());
+		builder.append("BackwardAlphabetic\t26        \t-                 ").append(System.lineSeparator());
+
+		Assert.assertEquals(builder.toString(), boas.toString());
+	}
+
+	@Test
+	public void testWriterNames() throws Exception {
+		final Haplotype hap1 = new Haplotype(new Sequence("ABCD"));
+		final Haplotype hap2 = new Haplotype(new Sequence("ABBD"));
+		hap2.setName("Hap");
+		final Haplotype hap3 = new Haplotype(new Sequence("ACCD"));
+		hap3.setName("VeryLongName");
+		final DistanceMatrix matrix = new DistanceMatrix(Arrays.asList(hap1, hap2, hap3));
+
+		final ByteArrayOutputStream boas = new ByteArrayOutputStream(200);
+		final DistanceMatrixWriter writer = new DistanceMatrixWriter(new OutputStreamWriter(boas));
+		writer.write(matrix);
+
+		final StringBuilder builder = new StringBuilder(200);
+		builder.append("HT-ID       \tnull\tHap \tVeryLongName").append(System.lineSeparator());
+		builder.append("null        \t-   \t1   \t1           ").append(System.lineSeparator());
+		builder.append("Hap         \t1   \t-   \t2           ").append(System.lineSeparator());
+		builder.append("VeryLongName\t1   \t2   \t-           ").append(System.lineSeparator());
+
+		Assert.assertEquals(builder.toString(), boas.toString());
+	}
+
+}
